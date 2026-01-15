31e1d7813e1081bcb425e11a6d9b8aa54ba4f190
nammaweb <nammaweb1@gmail.com>
2026-01-15 19:35:55 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Namma Web – Digital Marketing LMS</h1>
+      <p>Homepage is working correctly</p>
+    </div>
+  );
+}
