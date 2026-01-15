029734a68c0cd265e991f09b6992e8f38f63467c
nammaweb <nammaweb1@gmail.com>
2026-01-15 20:14:18 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Namma Web – Digital Marketing LMS</h1>
+      <p>Homepage is working successfully.</p>
+      <p>You can now proceed to build the full LMS.</p>
+    </div>
+  );
+}
