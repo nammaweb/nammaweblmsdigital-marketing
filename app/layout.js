cba587f17fcc852917e297ca43cd613b10c0cda5
nammaweb <nammaweb1@gmail.com>
2026-01-15 20:13:34 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Namma Web Digital Marketing LMS",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, fontFamily: "Arial, sans-serif" }}>
+        {children}
+      </body>
+    </html>
+  );
+}
