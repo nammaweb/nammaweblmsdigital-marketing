8de3115f87824e482391097aa5a680d0f90299c4
nammaweb <nammaweb1@gmail.com>
2026-01-16 11:32:19 +0530

    Create page.js

---
+++ app/test/day1/page.js
@@ -0,0 +1,9 @@
+export default function Day1() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>DAY 1 PAGE</h1>
+      <p>If you see this, routing is working.</p>
+      <a href="/test/day2">Go to Day 2</a>
+    </div>
+  );
+}
