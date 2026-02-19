453c3b65ee4ee4f4673e5f88ab7ec7ff2486b083
nammaweb <nammaweb1@gmail.com>
2026-02-19 13:13:24 +0530

    Delete app/test directory

--- app/test/day2/page.js
+++
@@ -1,9 +0,0 @@
-export default function Day2() {
-  return (
-    <div style={{ padding: "40px" }}>
-      <h1>DAY 2 PAGE</h1>
-      <p>This MUST be different from Day 1.</p>
-      <a href="/test/day1">Back to Day 1</a>
-    </div>
-  );
-}
