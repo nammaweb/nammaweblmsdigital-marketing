3fca710476dbfd800a9c0a62e0dca03979e1f484
nammaweb <nammaweb1@gmail.com>
2026-02-19 14:39:19 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,1 +1,13 @@
+export default function HomePage() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Namma Web Digital Marketing LMS</h1>
+      <p>Welcome to the official LMS platform.</p>
+
+      <a href="/login" style={{ color: "blue" }}>
+        Go to Login
+      </a>
+    </div>
+  );
+}
 
