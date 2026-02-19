201cc33f3594e1ebc84da5b19de9e15512acfaa5
nammaweb <nammaweb1@gmail.com>
2026-02-19 18:34:07 +0530

    Create finalExam.js

---
+++ data/finalExam.js
@@ -0,0 +1,10 @@
+export const finalExamQuestions = Array.from({ length: 50 }, (_, i) => ({
+  question: `Final Exam Question ${i + 1}: Advanced Digital Marketing Concept`,
+  options: [
+    "Correct strategic approach",
+    "Incorrect method",
+    "Outdated practice",
+    "Irrelevant action"
+  ],
+  correct: 0
+}));
