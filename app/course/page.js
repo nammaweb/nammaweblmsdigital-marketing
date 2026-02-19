3e06717bbd0878f39a0033b3a273f075200c1d98
nammaweb <nammaweb1@gmail.com>
2026-02-19 15:48:21 +0530

    Update page.js

--- app/course/page.js
+++ app/course/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { db } from "@/lib/firebase";
+import { db } from "../../lib/firebase";
 import { doc, getDoc } from "firebase/firestore";
 
 export default function CoursePage() {
