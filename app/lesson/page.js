4517330d0a88d819c46343e59d2721df38465923
nammaweb <nammaweb1@gmail.com>
2026-02-19 17:34:52 +0530

    Update page.js

--- app/lesson/page.js
+++ app/lesson/page.js
@@ -59,7 +59,28 @@ export default function Lesson() {
 
     setScore(calculatedScore);
 
-   if (calculatedScore >= 40 && Number(day) === Number(currentDay))
+   if (calculatedScore >= 40) {
+
+  const snap = await getDoc(doc(db, "students", name));
+  const data = snap.data();
+
+  if (Number(day) === Number(data.currentDay)) {
+
+    await updateDoc(doc(db, "students", name), {
+      currentDay: Number(data.currentDay) + 1,
+      totalScore: (data.totalScore || 0) + calculatedScore,
+      totalCompletedLessons: (data.totalCompletedLessons || 0) + 1,
+      rankPoints: (data.rankPoints || 0) + calculatedScore
+    });
+
+    alert("Next lesson unlocked!");
+    window.location.href = "/course";
+  }
+
+} else {
+  alert("Minimum 40/50 required.");
+}
+
 
       const snap = await getDoc(doc(db, "students", name));
       const data = snap.data();
